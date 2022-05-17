23e28e29f4867c8356a307a36aa199cf0690346f
chunoxie <michael.chunox@gmail.com>
2022-05-17 12:39:14 +0100

    Updated file with enter_score and print_score methods

--- student.py
+++ student.py
@@ -7,6 +7,9 @@ class Student:
         self.name = name
         self.age = age
         self.gender = gender
+        self.english_score = None
+        self.maths_score = None
+        self.science_score = None
 
     def speak(self):
         """
@@ -14,11 +17,30 @@ class Student:
         """
         print(f"Hello! My name is {self.name} and I'm {self.age} years old.")
 
-clement = Student("Clement", 25, 'Male')
-clement.speak()
+    def enter_score(self, english, maths, science):
+        self.english_score = english
+        self.maths_score = maths
+        self.science_score = science
 
+    def print_scores(self):
+        if self.english_score:
+            print(f'{self.name}, your scores are as follows:\n English: {self.english_score}\n Maths: {self.maths_score}\n Science: {self.science_score}\n ')
+        else:
+            print(f'Seems there is no score for {self.name}.\nYou need to enter scores first.\nUse the method enter_score()')
+
+        
 bennie = Student("Bennie", 21, 'Female')
-bennie.speak()
-
+clement = Student("Clement", 25, 'Male')
 samson = Student("Samson", 20, "Male")
+
+clement.speak()
+bennie.speak()
 samson.speak()
+
+samson.print_scores()
+samson.enter_score(50, 70, 80)
+samson.print_scores()
+clement.print_scores()
+
+
+
