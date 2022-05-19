d2762c54fac659ff269f123731842567d161d169
chunoxie <michael.chunox@gmail.com>
2022-05-19 12:09:34 +0100

    Continued with code refactor

--- register.py
+++ register.py
@@ -1,7 +1,12 @@
 from student import Student
 
-test_student = Student()
-test_student.speak()
+name = input("\nEnter name for new Student: ")
+age = input("Enter age for new student: ")
+gender = input("Enter student gender: ")
+
+test_student = Student(name, age, gender)
+print(f"\nHello! My name is {test_student.name} and I'm {test_student.age} years old.")
 
 test_student.enter_score()
-test_student.print_scores()
+
+print(f'\nSubjects for {test_student.name} are:\n{test_student.subjects}')
--- student.py
+++ student.py
@@ -5,21 +5,12 @@ This creates a student object.
 # We'll also see how to collaborate on GitHub
 # Today will be about making modules.
 class Student:
-    def __init__(self):
-        name = input("\nEnter name for new Student: ")
-        age = input("Enter age for new student: ")
-        gender = input("Enter student gender: ")
+    def __init__(self, name, age, gender):
         self.name = name
         self.age = age
         self.gender = gender
         self.subjects = {}
-
-    def speak(self):
-        """
-        Commands the student object to say its name and age.
-        """
-        print(f"\nHello! My name is {self.name} and I'm {self.age} years old.")
-
+        
     def enter_score(self):
         #new_subjects = {}
         number_of_subjects = int(input(f"\nHow many subjects do you want to register for {self.name}? "))
@@ -30,5 +21,3 @@ class Student:
             self.subjects[subject_name] = subject_score # what does this do please?
         #self.subjects = new_subjects
 
-    def print_scores(self):
-        print(f'\nSubjects for {self.name} are:\n{self.subjects}')
