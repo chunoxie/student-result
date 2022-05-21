e883449ad934c3b55084accc3c2e75947131edbb
chunoxie <michael.chunox@gmail.com>
2022-05-21 12:26:19 +0100

    Completed resultult computation in result.py

--- register.py
+++ register.py
@@ -10,8 +10,47 @@ print(f"\nHello! My name is {test_student.name} and I'm {test_student.age} years
 test_student.enter_score()
 
 print(f'\nSubjects for {test_student.name} are:\n{test_student.subjects}')
+scores_dict = test_student.subjects
+
+header = f"""
+===================================
+Welcome to TechApprentice Academy.
+FINAL RESULT SHEET.
+-----------------------------------
+Student Name:   {test_student.name}
+Student Age:    {test_student.age}
+Student Gender: {test_student.gender}
+====================================
+"""
+print(header)
+print('Subjects:')
+average_score = 0
+
+for x in scores_dict.keys():
+    y = scores_dict[x]
+    print(f'{x}: {y[-1]}')
+    average_score += y[-1]
+
+new_average = average_score / len(scores_dict)
+
+remarks = ''
+if new_average <= 40:
+    remarks = 'Poor'
+elif new_average > 40 and new_average <= 60:
+    remarks = 'Average'
+else:
+    remarks = 'Good'
+
+footer = f"""
+====================================
+Average: {new_average}
+Comment: {remarks}
+===================================
+"""
+print(footer)
 
 """
+Sample output when printed:
 ===================================
 Welcome to TechApprentice Academy.
 FINAL RESULT SHEET.
